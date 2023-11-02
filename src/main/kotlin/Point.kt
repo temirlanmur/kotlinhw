ddc35388f9d996f891610a6b8658913f85c2c866
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2023-11-03 00:39:02 +0600

    restructure project folder

--- src/main/kotlin/Point.kt
+++
@@ -1,9 +0,0 @@
-class Point(val x: Int, val y: Int) {
-    fun isCollinear(a: Point, b: Point): Boolean {
-        return (a.y - b.y) * (a.x - x) == (a.y - y) * (a.x - b.x)
-    }
-
-    override fun toString(): String {
-        return "($x, $y)"
-    }
-}
